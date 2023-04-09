875a14f3c6d485657a9708bf5ba7b49ccfbb3f7f
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-04-09 01:07:54 -0400

    Updates to the Matlab implementation which allow for file compression and training matrix creation

---
+++ MatLabImplementation/svsExtraction.m
@@ -0,0 +1,23 @@
+function [] = svsExtraction(folderPath, cancerType)
+
+filePattern = fullfile(folderPath, "*.svs");
+theFiles= dir(filePattern);
+
+columnVector = cell(length(theFiles), 3);
+
+for i = 1 : length(theFiles)
+    fullFileName = fullfile(theFiles(i).folder, theFiles(i).name);
+    
+    io = imread(fullFileName, 'Index', 3);
+    
+    columnVector{i,1} = theFiles(i).name;
+    columnVector{i,2} = io;
+    
+    % TODO: This needs to parse each line of the manifest file and return
+    % the last entry, which is the label for the data.
+    % columnVector{i,3} = 'done';
+end
+
+assignin('base', cancerType + 'columnVector', columnVector);
+
+end
