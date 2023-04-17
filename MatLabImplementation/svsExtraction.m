a7f5836754cdbdb2b5f423e98869a1a3c29c00ef
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-04-17 12:47:35 -0400

    Update feature extraction file

--- MatLabImplementation/svsExtraction.m
+++ MatLabImplementation/svsExtraction.m
@@ -5,27 +5,30 @@ filePattern = fullfile(folderPath, "*.svs");
 theFiles= dir(filePattern);
 manifestFile = readtable(manifestPath, Delimiter='\t', ReadVariableNames=true);
 
-columnVector = cell(length(theFiles), 3);
+columnVector = cell(length(theFiles), 2);
 
 %% Image extraction
 for i = 1 : length(theFiles)
     fullFileName = fullfile(theFiles(i).folder, theFiles(i).name);
+    fileName = theFiles(i).name;
+    savedAs = strcat("/Volumes/NolansDrive/TCGA-CNN/Breast/breastCancerImages/", extractBefore(fileName, length(fileName)-3), ".png");
     
     io = imread(fullFileName, 'Index', 3);
-    
-    fileName = theFiles(i).name;
+    imwrite(io, savedAs);
 
-    columnVector{i,1} = fileName;
-    columnVector{i,2} = io;
+    columnVector {i,1} = fileName;
+    %BreastColumnVector {i,1} = fileName;
+    %BreastColumnVector {i,2} = io;
     
     %% Labeling
-    columnVector{i,3} = string(table2cell(manifestFile(strcmp(manifestFile{:,2}, fileName), 6)));
-
+    columnVector {i,2} = string(table2cell(manifestFile(strcmp(manifestFile{:,2}, fileName), 6)));
+    disp(i)
     % columnVector{i,3} = 'done';
 end
 
-datasetName = cancerType + 'ColumnVector';
+datasetName = cancerType + 'Labels';
 assignin('base', datasetName, columnVector);
-save(datasetName + ".mat", datasetName)
+%outputName = strcat("/Volumes/NolansDrive/TCGA-CNN/", datasetName, ".mat");
+%save(outputName, "columnVector");
 
 end
