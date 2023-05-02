0ff0da4c2fe7aa18e2f5d94bdbfad4392dd9df71
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-05-02 17:41:46 -0400

    Update createAugmentedImages.m
    Updated

--- MatLabImplementation/createAugmentedImages.m
+++ MatLabImplementation/createAugmentedImages.m
@@ -1,30 +1,31 @@
-function [] = createAugmentedImages(originalImages)
+%function [] = augmentedImages(originalImages)
 
 %%
+
+originalImages = 'E:\TCGA-CNN\commonCancerDataset';
 if ~exist(strcat(originalImages, '/augmentedImages'), 'dir')
     mkdir(strcat(originalImages, '/augmentedImages'));
 end
-outputPath = '/Users/nolantremelling/Downloads/commonCancerDataset/augmentedImages';
-primaryTumorPath = dir(strcat(originalImages, '/primaryTumor'));
-solidTissueNormalPath = dir(strcat(originalImages, '/solidTissueNormal'));
+outputPath = 'E:\TCGA-CNN\commonCancerDataset\augmentedImages';
+primaryTumorPath = dir(strcat(originalImages, '/primaryTumor/*.png'));
+solidTissueNormalPath = dir(strcat(originalImages, '/solidTissueNormal/*.png'));
 
 primaryTumorCleanImages = dir(strcat(originalImages, '/primaryTumor/*.png'));
 solidTissueNormalCleanImages = dir(strcat(originalImages, '/solidTissueNormal/*.png'));
 
-if ~exist(strcat(outputPath, '/augmentedImages/primaryTumor'), 'dir')
-    mkdir(strcat(outputPath, '/augmentedImages/primaryTumor'));
-end
-
-if ~exist(strcat(outputPath, '/augmentedImages/solidTissueNormal'), 'dir')
-    mkdir(strcat(outputPath, '/augmentedImages/solidTissueNormal'));
-end
+% if ~exist(strcat(outputPath, '/primaryTumor'), 'dir')
+%     mkdir(strcat(outputPath, '/primaryTumor'));
+% end
+% 
+% if ~exist(strcat(outputPath, '/solidTissueNormal'), 'dir')
+%     mkdir(strcat(outputPath, '/solidTissueNormal'));
+% end
 
 primaryTumorAugmentedImages = dir(strcat(outputPath, '/primaryTumor'));
 solidTissueNormalAugmentedImages = dir(strcat(outputPath, '/solidTissueNormal'));
 
 for i = 1:numel(primaryTumorCleanImages)
-    name = image_files(i).name;
-    image = imread(fullfile(primaryTumorPath, image_files(i).name));
+    image = imread(fullfile(originalImages, 'primaryTumor\', primaryTumorCleanImages(i).name));
     
     tform1 = randomAffine2d(Rotation=[0 45]);
     image = imwarp(image,tform1);
@@ -35,17 +36,17 @@ for i = 1:numel(primaryTumorCleanImages)
     tform3 = randomAffine2d(YShear=[-45 45]);
     image = imwarp(image, tform3);
 
-    image = jitterColorHSV(image,'Contrast',[-1 1],'Hue',[-1 1],'Saturation',[-1 1],'Brightness',[-1 1]);
+    image = jitterColorHSV(image,'Contrast',1,'Hue',1,'Saturation',1,'Brightness',1);
 
     sigma = 1+5*rand;
     image= imgaussfilt(image,sigma); 
 
-    imwrite(image, fullfile(primaryTumorAugmentedImages , name, 'augId1'))
+    imwrite(image, fullfile(outputPath, 'primaryTumor\' , name, 'augId1.png'));
 end
 
 for i = 1:numel(solidTissueNormalCleanImages)
-    name = image_files(i).name;
-    image = imread(fullfile(solidTissueNormalPath, image_files(i).name));
+    name = solidTissueNormalCleanImages(i).name;
+    image = imread(fullfile(originalImages, 'solidTissueNormal\', solidTissueNormalCleanImages(i).name));
     
     tform1 = randomAffine2d(Rotation=[0 45]);
     image = imwarp(image,tform1);
@@ -56,10 +57,10 @@ for i = 1:numel(solidTissueNormalCleanImages)
     tform3 = randomAffine2d(YShear=[-45 45]);
     image = imwarp(image, tform3);
 
-    image = jitterColorHSV(image,'Contrast',[-1 1],'Hue',[-1 1],'Saturation',[-1 1],'Brightness',[-1 1]);
+    image = jitterColorHSV(image,'Contrast',1,'Hue',1,'Saturation',1,'Brightness',1);
 
     sigma = 1+5*rand;
     image= imgaussfilt(image,sigma); 
 
-    imwrite(image, fullfile(solidTissueNormalAugmentedImages , name, 'augId1'))
+    imwrite(image, fullfile(outputPath, 'solidTissueNormal\' , name, 'augId1.png'));
 end
